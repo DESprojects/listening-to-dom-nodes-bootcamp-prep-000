ae5c724f4343a02df978d308197e497b3a51a4f4
Charles Eul-Stacy <myherokelly@gmail.com>
2018-04-02 05:54:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const main = document.getElementById('main');
+const input = document.querySelector('input');
+
 
 main.addEventListener('click', function(e){
   alert('I was clicked');
+})
+
+input.addEventListener('keydown', function(e){
+  alert(e.which);
 })
