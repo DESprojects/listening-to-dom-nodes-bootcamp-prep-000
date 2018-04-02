712bf48caa4460e9d2f8227c3bbbf598c2fd236e
Charles Eul-Stacy <myherokelly@gmail.com>
2018-04-02 05:58:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const main = document.getElementById('main');
 const input = document.querySelector('input');
-
+let divs = document.querySelectorAll('div');
 
 main.addEventListener('click', function(e){
   alert('I was clicked');
@@ -11,3 +11,11 @@ input.addEventListener('keydown', function(e){
     return e.preventDefault();
   }
 })
+
+function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + 'bubbled');
+}
+
+for(let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
